e1a6e06d6eb271dc840c617decb5b0baac4065cd
varkenvarken <michel@michelanders.nl>
2019-12-15 16:11:39 +0100

    initial commit

---
+++ Makefile
@@ -0,0 +1,31 @@
+SYN = yosys
+PNR = nextpnr-ice40
+GEN = icepack
+PROG = iceprog 
+
+TOP = robin.v
+PCF = icebreaker.pcf
+DEVICE = --up5k
+PACKAGE= sg48
+PLACER=heap
+
+OUTPUT = $(patsubst %.v,%.bin,$(TOP))
+
+all: $(OUTPUT)
+
+%.bin: %.asc
+	$(GEN) $< $@
+
+%.asc: %.json $(PCF)
+	$(PNR) $(DEVICE) --placer $(PLACER) --package $(PACKAGE) --json $< --pcf $(PCF) --asc $@
+
+%.json: $(TOP) $(PCF)
+	$(SYN) -q -p "read_verilog $<; hierarchy -libdir . ; synth_ice40 -flatten -json $@"
+
+clean:
+	rm -f *.bin *.blif *.tiles *.asc *.json
+
+flash: $(OUTPUT)
+	$(PROG) $<
+
+.PHONY: all clean flash
---
+++ robin.v
@@ -0,0 +1,34 @@
+/* robin, a SoC design for the IceBreaker board.
+ *
+ * Copyright 2019 Michel Anders
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+
+
+`define ADDR_WIDTH 12
+
+module top(
+	input CLK,
+	input RX,
+	output TX,
+	output LED1,
+	output LED2,
+	output LED3,
+	output LED4,
+	output LED5,
+	output BTN_N,
+	output LEDR_N,
+	output LEDG_N);
+
+endmodule
