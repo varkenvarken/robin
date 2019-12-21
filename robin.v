b287c5ccd6f835084c6a6302dce0ca0a0d7b1f5b
varkenvarken <michel@michelanders.nl>
2019-12-21 12:50:33 +0100

    uart with single fifo

--- robin.v
+++ robin.v
@@ -15,13 +15,9 @@
  * limitations under the License. 
  */
 
-
-`include "../puck/cores/osdvu/uart.v"
+`include "../puck/cores/osdvu/uart.v"  // outside the hierarchy so we have to include
 
 `default_nettype	none
-
-`define ADDR_WIDTH 12
-
 
 module top(
 	input CLK,
@@ -36,6 +32,10 @@ module top(
 	output reg LEDR_N,
 	output reg LEDG_N);
 
+	// global reset active low, cleared after startup
+	reg reset = 0;
+	always @(posedge CLK) reset <= 1;
+
 	// uart wires
 	wire u_reset = 0;
 	reg [7:0] u_tx_byte;
@@ -43,13 +43,17 @@ module top(
 	reg u_transmit;
 	wire u_received,u_is_transmitting;
 	wire u_break,u_error;
+	wire u_reset;
+	assign u_reset = ~reset; // uart reset is active high
+
+	localparam ADDR_WIDTH = 4;
 
 	uart #(
 		.baud_rate(115200),
 		.sys_clk_freq(12000000)
 	) uart0 (
 		.clk(CLK),							// The master clock for this module
-		.rst(0),							// Synchronous reset
+		.rst(u_reset),						// Synchronous reset
 		.rx(RX),							// Incoming serial line
 		.tx(TX),							// Outgoing serial line
 		.transmit(u_transmit),				// Assert to begin transmission
@@ -61,15 +65,50 @@ module top(
 		.recv_error(u_error)      			// Indicates error in receiving packet.
 		// output reg [3:0] rx_samples,
 		// output reg [3:0] rx_sample_countdown
-);
+	);
+
+	reg [ADDR_WIDTH-1:0] buf_waddr, buf_raddr;
+	reg [7:0] buf_din;
+	reg buf_write_en;
+	wire [7:0] buf_dout;
+
+	ram #(.addr_width(ADDR_WIDTH))
+	buffer(
+		.din		(buf_din), 
+		.write_en	(buf_write_en), 
+		.waddr		(buf_waddr), 
+		.wclk(CLK), 
+		.raddr		(buf_raddr), 
+		.rclk(CLK),
+		.dout		(buf_dout)
+	);
+
+	reg [ADDR_WIDTH-1:0] next_waddr, next_raddr;
+
+	reg [ADDR_WIDTH-1:0] bytes_received;
+	wire bytes_received_nonzero;
+	assign bytes_received_nonzero = bytes_received != 0;
 
 	always @(posedge CLK) begin
-		u_transmit <= 0;
-		LED1 <= 0;
+		buf_waddr <= next_waddr;
+		buf_raddr <= next_raddr;
+	end
+
+	always @(posedge CLK) begin
+		// store incoming
 		if(u_received) begin
-			u_tx_byte <= u_rx_byte;
-			LED1 <= 1;
+			buf_din <= u_rx_byte;
+			buf_write_en <= 1;
+			next_waddr <= buf_waddr + 1;
+			bytes_received <= bytes_received + 1;
+		end
+		// transmit stored bytes
+		u_transmit <= 0;
+		if(~u_is_transmitting & bytes_received_nonzero) begin
+			u_tx_byte <= buf_dout;
 			u_transmit <= 1;
+			next_raddr <= buf_raddr + 1;
+			bytes_received <= bytes_received - 1;
 		end
 	end
 
