848f4893b14d26a61d619195548f3e4e56a576ab
varkenvarken <michel@michelanders.nl>
2020-01-02 18:03:29 +0100

    initial version of a division module

--- Makefile
+++ Makefile
@@ -6,9 +6,9 @@ PROG = iceprog
 TOP = robin.v
 PCF = icebreaker.pcf
 DEVICE = --up5k
-PACKAGE= sg48
-PLACER=heap
-
+PACKAGE = sg48
+PLACER = heap
+PLACERLOG = placer.log
 OUTPUT = $(patsubst %.v,%.bin,$(TOP))
 
 all: $(OUTPUT)
@@ -17,9 +17,9 @@ all: $(OUTPUT)
 	$(GEN) $< $@
 
 %.asc: %.json $(PCF)
-	$(PNR) $(DEVICE) --placer $(PLACER) --package $(PACKAGE) --json $< --pcf $(PCF) --asc $@
+	$(PNR) $(DEVICE) --placer $(PLACER) --package $(PACKAGE) --quiet --log $(PLACERLOG) --json $< --pcf $(PCF) --asc $@
 
-%.json: $(TOP) debounce.v fifo.v ram.v cpu.v alu.v $(PCF) Makefile
+%.json: $(TOP) debounce.v fifo.v ram.v cpu.v alu.v divider.v $(PCF) Makefile
 	$(SYN) -q -p "read_verilog $<; hierarchy -libdir . ; synth_ice40 -dsp -flatten -json $@"
 
 clean:
---
+++ divider.v
@@ -0,0 +1,91 @@
+/* robin, a SoC design for the IceBreaker board.
+ *
+ * divider.v : a 32 bit integer division module
+ *
+ * Copyright 2019,2020 Michel Anders
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+ 
+ module divider(
+    input clk,
+    input reset,
+	input [31:0] a,
+	input [31:0] b,
+	input go,
+	input divs,
+	output [31:0] c,
+	output is_zero,
+	output is_negative,
+	output reg available
+	);
+
+	localparam DIV_SHIFTL    = 0;
+	localparam DIV_SUBTRACT  = 1;
+	localparam DIV_AVAILABLE = 2;
+	localparam DIV_DONE      = 3;
+	reg [2:0] step;
+
+	reg [31:0] dividend;
+	reg [31:0] divisor;
+	reg [31:0] quotient, quotient_part;
+	wire overshoot = divisor > dividend;
+	wire division_by_zero = (b == 0);
+
+	wire [31:0] result = quotient;
+
+	always @(posedge clk) begin
+		if(go) begin
+			step <= DIV_SHIFTL;
+			available <= 0;
+			dividend <= a;
+			divisor <= b;
+			quotient <= 0;
+			quotient_part <= 1;
+		end else
+			case(step)
+				DIV_SHIFTL	: 	begin
+									if(~overshoot) begin
+										divisor <= divisor << 1;
+										quotient_part <= quotient_part << 1;
+									end else begin
+										divisor <= divisor >> 1;
+										quotient_part <= quotient_part >> 1;
+										step <= DIV_SUBTRACT;
+									end
+								end
+				DIV_SUBTRACT:	begin
+									if(quotient_part == 0)
+										step <= DIV_AVAILABLE;
+									else begin
+										if(~overshoot) begin
+											dividend <= dividend - divisor;
+											quotient <= quotient | quotient_part;
+										end 
+										divisor <= divisor >> 1;
+										quotient_part <= quotient_part >> 1;
+									end
+								end
+				DIV_AVAILABLE:	begin
+									step <= DIV_DONE;
+									available <= 1;
+								end
+				default		: 	available <= 0;
+			endcase
+	end
+
+	assign c = result[31:0];
+	assign is_zero = (c == 0);
+	assign is_negative = c[31];
+
+endmodule
