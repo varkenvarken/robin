93acda3a4afb0b6a85b59777da5030755194e6c1
varkenvarken <michel@michelanders.nl>
2020-01-29 16:15:26 +0100

    tweaks to the automated tests

--- libc/stdlib/Test/test_ftoa.s
+++ libc/stdlib/Test/test_ftoa.s
@@ -6,12 +6,21 @@ start:  0x200
     push    r2
     call    #ftoa
     mover   sp,sp,3 ; pop link and args
+
     loadl   r2,#number_2
     push    r2
     loadl   r2,#0xc0600000  ; -3.5
     push    r2
     call    #ftoa
     mover   sp,sp,3 ; pop link and args
+
+    loadl   r2,#number_3
+    push    r2
+    loadl   r2,#0x3f9e0419  ; 1.2345
+    push    r2
+    call    #ftoa
+    mover   sp,sp,3 ; pop link and args
+
     halt
     
     stackdef
@@ -19,4 +28,6 @@ start:  0x200
 number_1:
     byte0 "               " ; 15 spaces + \0
 number_2:
+    byte0 "               " ; 15 spaces + \0
+number_3:
     byte0 "               " ; 15 spaces + \0
